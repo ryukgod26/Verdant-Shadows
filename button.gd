630f6f9876036af97c4a8d207963a3d107a1f1f4
Shashwat Dubey <shashwat5590@gmail.com>
2026-01-31 10:49:15 +0800

    added fonts, improve main menu UI, button adjustment shi idk

--- button.gd
+++ button.gd
@@ -1,14 +1,14 @@
 extends Button
 
-
+var SCALE_ON_HOVER = 1.02
 
 func _on_button_mouse_entered() -> void:
 	pivot_offset = size/2
 	var tween = create_tween()
-	tween.tween_property(self,"scale",Vector2(1.2,1.2),0.2)
+	tween.tween_property(self,"scale",Vector2(SCALE_ON_HOVER, SCALE_ON_HOVER),0.1)
 
 
 func _on_button_mouse_exited() -> void:
 	var tween = create_tween()
-	tween.tween_property(self,"scale",Vector2(1.,1.),0.2)
+	tween.tween_property(self,"scale",Vector2(1.0,1.0),0.1)
 	
